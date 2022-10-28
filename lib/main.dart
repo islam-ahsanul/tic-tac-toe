7b32a7c2311ab56f858228adc19e1847095bb88c
islam-ahsanul <ahsanul.islam@g.bracu.ac.bd>
2022-10-28 10:40:36 +0600

    Created grids

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
@@ -35,7 +25,26 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: const Text('Game Page')),
-    );
+        appBar: AppBar(
+          title: Text('Tic-Tac-Toe'),
+        ),
+        body: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: [for (var i = 0; i < 9; i++) Text('X')],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Your Move'),
+                TextButton(onPressed: () {}, child: Text('Restart')),
+              ],
+            )
+          ],
+        ));
   }
 }

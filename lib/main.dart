b37c0d9a0faa9050f88a2aefea296d3f8aaa09d3
islam-ahsanul <ahsanul.islam@g.bracu.ac.bd>
2022-10-28 11:18:01 +0600

    Implemented Ai

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var tiles = List.filled(9, 0);
 
   @override
   Widget build(BuildContext context) {
@@ -34,17 +41,86 @@ class MyHomePage extends StatelessWidget {
               aspectRatio: 1,
               child: GridView.count(
                 crossAxisCount: 3,
-                children: [for (var i = 0; i < 9; i++) Text('X')],
+                children: [
+                  for (var i = 0; i < 9; i++)
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          tiles[i] = 1;
+                          runAi();
+                        });
+                      },
+                      child: Center(
+                        child: Text(tiles[i] == 0
+                            ? ''
+                            : tiles[i] == 1
+                                ? 'X'
+                                : '0'),
+                      ),
+                    )
+                ],
               ),
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('Your Move'),
-                TextButton(onPressed: () {}, child: Text('Restart')),
+                Text(isWinning(1, tiles)
+                    ? 'You Won!'
+                    : isWinning(2, tiles)
+                        ? 'You Lost!'
+                        : 'Your move'),
+                TextButton(
+                    onPressed: () {
+                      setState(() {
+                        tiles = List.filled(9, 0);
+                      });
+                    },
+                    child: Text('Restart')),
               ],
             )
           ],
         ));
+  }
+
+  void runAi() async {
+    await Future.delayed(Duration(milliseconds: 200));
+
+    int? winning;
+    int? blocking;
+    int? normal;
+
+    for (var i = 0; i < 9; i++) {
+      var val = tiles[i];
+      if (val > 0) {
+        continue;
+      }
+      var future = [...tiles]..[i] = 2;
+      if (isWinning(2, future)) {
+        winning = i;
+      }
+      if (isWinning(1, future)) {
+        blocking = 1;
+      }
+      normal = i;
+    }
+
+    var move = winning ?? blocking ?? normal;
+
+    if (move != null) {
+      setState(() {
+        tiles[move] = 2;
+      });
+    }
+  }
+
+  bool isWinning(int who, List<int> tiles) {
+    return (tiles[0] == who && tiles[1] == who && tiles[2] == who) ||
+        (tiles[3] == who && tiles[4] == who && tiles[5] == who) ||
+        (tiles[6] == who && tiles[7] == who && tiles[8] == who) ||
+        (tiles[0] == who && tiles[4] == who && tiles[8] == who) ||
+        (tiles[2] == who && tiles[4] == who && tiles[6] == who) ||
+        (tiles[0] == who && tiles[3] == who && tiles[6] == who) ||
+        (tiles[1] == who && tiles[4] == who && tiles[7] == who) ||
+        (tiles[2] == who && tiles[5] == who && tiles[8] == who);
   }
 }
